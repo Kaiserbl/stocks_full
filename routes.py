81212559bf1b834dae1f3bc4526ecfb7aa35c010
Pedro Brochero <pedrobrochero@gmail.com>
2021-10-14 11:24:33 -0500

    Added edit product page

--- routes.py
+++ routes.py
@@ -5,6 +5,10 @@ def setRoutes(app):
     def index():
         return render_template('index.html')
 
+    @app.route("/products")
+    def editProduct():
+        return render_template('product_edit.html')
+
     @app.errorhandler(404)
     def error_404_handler(e):
         return render_template('404.html'), 404
