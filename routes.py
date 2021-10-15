de1db2954a9a1221af14228e3d835fb3e1df094e
kaiserbl <juancamilomanrrique34@gmail.com>
2021-10-14 20:12:01 -0500

    details

--- routes.py
+++ routes.py
@@ -12,3 +12,15 @@ def setRoutes(app):
     @app.errorhandler(404)
     def error_404_handler(e):
         return render_template('404.html'), 404
+    
+    @app.route('/product/detail')
+    def home():
+        return render_template('product_detail.html')
+    
+    @app.route('/provider/detail')
+    def productos():
+        return render_template('provider_detail.html')
+
+    @app.route('/user/detail')
+    def servicios():
+        return render_template('user_detail.html')
