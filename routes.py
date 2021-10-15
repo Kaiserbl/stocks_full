4e718eba745383db079537ee5c861f79ad7c874c
Pedro Brochero <pedrobrochero@gmail.com>
2021-10-14 20:00:34 -0500

    Added sidebar. Added edit provider. Added edit user.

--- routes.py
+++ routes.py
@@ -7,7 +7,15 @@ def setRoutes(app):
 
     @app.route("/products")
     def editProduct():
-        return render_template('product_edit.html')
+        return render_template('product_edit.html', name='Muscle car model 7')
+
+    @app.route("/users")
+    def editUser():
+        return render_template('user_edit.html', name='Sharon Hernandez')
+
+    @app.route("/providers")
+    def editProvider():
+        return render_template('provider_edit.html', name='Santorini')
 
     @app.errorhandler(404)
     def error_404_handler(e):
