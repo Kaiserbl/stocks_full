f13a69773d4d481e86171c011fcc7e1260037e4a
gregory machacon <wawandco@MacBook-Pro-de-gregory.local>
2021-10-14 22:49:11 -0500

    created search pages

--- routes.py
+++ routes.py
@@ -12,3 +12,15 @@ def setRoutes(app):
     @app.errorhandler(404)
     def error_404_handler(e):
         return render_template('404.html'), 404
+
+    @app.route("/products/search")
+    def searchProduct():
+        return render_template('search_products.html')
+
+    @app.route("/users/search")
+    def searchUsers():
+        return render_template('search_users.html')
+    
+    @app.route("/providers/search")
+    def searchProviders():
+        return render_template('search_providers.html')
